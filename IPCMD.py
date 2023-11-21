fca1202c3cb8f1939f2c1b035a8ae0dd19ecdbce
Nate Goodman <nategoodman88@gmail.com>
2023-11-21 09:45:57 -0500

    Update IPCMD.py

--- IPCMD.py
+++ IPCMD.py
@@ -1,12 +1,21 @@
 #Allows use of Windows commands
 import os 
 #Ask IP that is troubleshooting
-print('Enter the IP you want to ping and trace')
-ip = input()
-#Ping and traces IP
-print('Pinging...')
-os.system(f'ping {ip}')
-print('\nPing complete. Tracing...')
-os.system(f'tracert {ip}')
-print('\nTrace complete. Done.')
-
+working = True
+while(working):
+  print('Enter the IP you want to ping and trace.')
+  ip = input()
+  #Ping, traces and ties to find dns host 
+  print('Pinging...')
+  os.system(f'ping {ip}')
+  print('\nPing complete. Tracing...')
+  os.system(f'tracert {ip}')
+  print('\nTrace complete. Trying to find dns host...')
+  os.system(f'nslookup {ip}')
+  print('\nDNS Trace complete.')
+  print('\nPress n to troubleshoot another IP. Type anything else to quit.')
+  choice = input()
+  if choice == 'n' or choice == 'N':
+    working = True
+  else:
+    working = False
